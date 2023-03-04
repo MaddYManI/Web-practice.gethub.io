dd2b815cdf23cfa4fa1997d7ae940f59ee662c19
Manish <manishdhanapal21@gmail.com>
2023-03-04 09:04:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <button onclick="alert('Are you sure you want to continue?')"> 
+        <img src="https://yourserver.com/button_img.jpg" 
+          alt="Submit the form" height="64" width="64"/>
+     </button> 
+    </div>
+  );
+}
+
+export default App; 
