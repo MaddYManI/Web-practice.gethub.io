17f0982ff4388e4cfb248fd558b5dcf3043753c5
Manish <manishdhanapal21@gmail.com>
2023-03-17 08:06:07 +0530

    navbar hide update

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,7 @@ import './Footer.css'
 function Footer (){
     return(
         <footer className='box' >
-            <div className='box2'>
-                <ul className='footer1'><b>Navigation</b>
+                  <ul className='navigation'><b>Navigation</b>
                     <li className='footer2'>Home</li>
                     <li className='footer2'>About</li>
                     <li className='footer2'>menu</li>
@@ -12,19 +11,18 @@ function Footer (){
                     <li className='footer2'>Order Online</li>
                     <li className='footer2'>Login</li>
                 </ul>
-                <ul className='footer1'><b>Contact</b>
+                <ul className='contact'><b>Contact</b>
                     <li className='footer2'>Address</li>
                     <li className='footer2'>Phone number</li>
                     <li className='footer2'>Email</li>
                 </ul>
-                <ul className='footer1'><b>Social Media Links</b>
+                <ul className='socialmedia'><b>Social Media Links</b>
                     <li className='footer2'>Facebook</li>
                     <li className='footer2'>Twitter</li>
                     <li className='footer2'>Whatsapp</li>
                 </ul>
-            </div>
         </footer>
-    )
+      )
 
 }
 export default Footer;
--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,27 @@
-import logo from "./Logo.png"
+import logo from "./Logo .svg"
 import './Nav.css'
 
 function Nav (){
     return(
-        <>
+        <header className="box2">
+            <input type="checkbox" id="toggle"/>
             <nav className="containers">
+                <label className="navbar-toggle" for="toggle">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </label>
                 <img src={logo} alt ="Logo" />
-                <ul>
-                    <li><b><a href="">Home</a></b></li>
-                    <li><b><a href="#">About</a></b></li>
-                    <li className="test" ><b><a href="#">Menu</a></b></li>
-                    <li><b><a href="#">Reservation</a></b></li>
-                    <li><b><a href="#">Order Online</a></b></li>
-                    <li><b><a href="#">Login</a></b></li>
+                <ul className="lists">
+                    <li className="list1"><b><a href="#">Home</a></b></li>
+                    <li className="list1"><b><a href="#">About</a></b></li>
+                    <li className="list1"><b><a href="#">Menu</a></b></li>
+                    <li className="list1"><b><a href="#">Reservation</a></b></li>
+                    <li className="list1"><b><a href="#">Order Online</a></b></li>
+                    <li className="list1"><b><a href="#">Login</a></b></li>
                 </ul>
             </nav>
-        </>
+        </header>
     )
 }
 export default Nav;
