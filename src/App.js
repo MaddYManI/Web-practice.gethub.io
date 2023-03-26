9047417d23ccfaecfe81d7deaf575899eca4772e
Manish <manishdhanapal21@gmail.com>
2023-03-26 16:22:18 +0530

    Implemented BrowserRouter and Route.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import { Routes,Route} from 'react-router-dom'
-import Header from './Header';
 import Nav from './Nav';
-import Body from './Body';
-import Main from './Main';
-import Footer from './Footer';
 
 function App() {
   return (
     <>
-      <Header/>
       <Nav/>
-      <Body/>
-      <Main/>
-      <Footer/>
     </>
   );
 }
