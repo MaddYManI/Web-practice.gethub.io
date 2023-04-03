62dd05d2caab7b053e899e7935b757700643434b
Manish <manishdhanapal21@gmail.com>
2023-04-03 19:51:41 +0530

    Assets upload

--- src/Nav/Reservation.js
+++ src/Nav/Reservation.js
@@ -1,4 +1,29 @@
+import './Reservation.css'
+
 function Reservation () {
-    return;
+    const [availableTimes , setavailableTimes ] = useState('17:00');
+    return(
+        <form className='reservation_details'>
+            <label for="res-date">Choose date</label>
+            <input type="date" id="res-date"/>
+            <label for="res-time">Choose time</label>
+            <select id="res-time " onChange={setavailableTimes}>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </select>
+            <label for="guests">Number of guests</label>
+            <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+            <label for="occasion">Occasion</label>
+            <select id="occasion">
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </select>
+            <input type="submit" value="Make Your reservation"/>
+        </form>
+    );
 }
 export default Reservation;
